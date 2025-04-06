c35176eadce48b49d46096bbdaad1f4e798d8aa4
EmanuelGarcia <emanuelgarciarios@gmail.com>
2025-04-05 23:13:22 -0500

    Added traverse method

--- src/model/music_player.py
+++ src/model/music_player.py
@@ -8,16 +8,40 @@ class Song:
         return f"{self.title} - {self.artist} - ({self.duration})"
 
 class Node:
-    def __init__(self, value: Song, next: Song = None, prev: Song = None):
-        self.value: Song = value
+    def __init__(self, song: Song, next: Song = None, prev: Song = None):
+        self.song: Song = song
         self.next: Song = next
         self.prev: Song = prev
     
     def __repr__(self):
-        return f"🎵 {self.value.title} < - > 🎵{self.next.title}"
+        return f"🎵 {self.song.title} < - > 🎵{self.next.title}"
     
-class DoubleLinkedList:
+class Playlist:
+    #Double linked list
     def __init__(self, head: Node = None, tail: Node = None):
         self.__head: Node = head
         self.__tail: Node = tail
         self.__size: int = 0
+    
+    def add_song(self, title: str, artist: str, duration: float):
+        new_song = Song(title, artist, duration)
+        new_node = Node(new_song)
+        if (self.__size == 0):
+            self.__head = new_node
+            self.__tail = new_node
+        else:
+            new_node.prev = self.__tail
+            self.__tail.next = new_node
+            self.__tail = new_node
+        self.__size += 1
+    
+    def traverse(self):
+        current_node = self.__head
+        while (current_node is not None):
+            print(current_node.song)
+            current_node = current_node.next
+
+playlist = Playlist()
+playlist.add_song("Treat you better", "Shawn Mendes", 10)
+playlist.add_song("When i was your man", "Bruno Mars", 10)
+playlist.traverse()
